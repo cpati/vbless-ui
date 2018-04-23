d5cef2fef632c98978eb5f57ed5662e2817780a3
pujakb <pujakb1987@gmail.com>
2018-04-22 22:59:27 -0700

    create campaign + edit campaign flow, known issue - date not being populated in edit campaign page

--- viewModels/app.js
+++ viewModels/app.js
@@ -2,3 +2,13 @@
 
 var myApp = angular.module('myApp', [ 'ngRoute']);
 
+myApp.run(['$rootScope', function($rootScope) {
+	$rootScope.test="ABC";
+  $rootScope.tenantId=100;
+
+		  $rootScope.userInfo={
+			"userName":"creator@vBless.onmicrosoft.com",
+			"isAdmin":"true"
+		};
+
+    }]);
